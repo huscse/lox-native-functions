d07b8b5845f58624da3e3009e12714fb10404352
huscse <husnainkhaliq82@gmail.com>
2025-12-12 23:03:37 -0500

    add quicksort psuedocode

--- lox.java
+++ lox.java
@@ -129,3 +129,22 @@ public Object visitWhileStmt(Stmt.While stmt) {
 
 //     if match:
 //         return "Match found at: ", i
+
+
+// QUICKSORT(A, low, high):
+// if low < high:
+//     p = PARTITION(A, low, high)
+//     QUICKSORT(A, low, p - 1)
+//     QUICKSORT(A, p + 1, high)
+
+// PARTITION(A, low, high):
+//     pivot = A[high]
+//     i = low - 1
+
+//     for j = low to high - 1:
+//         if A[j] <= pivot:
+//             i = i + 1
+//             SWAP A[i] with A[j]
+
+//     SWAP A[i + 1] with A[high]
+//     return i + 1
